3b92638dddd23bac7658e9572cd270e6d448ee13
rmx <maxim.romin@gmail.com>
2016-02-04 08:22:11 -0500

    file structure; gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+require('es6-promise').polyfill();
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    rename = require('gulp-rename'),
+    cmq = require('gulp-combine-media-queries');
+
+gulp.task('sass', function() {
+    return gulp.src('./assets/scss/style.scss')
+        .pipe(sass())
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(cmq())
+        .pipe(cssnano())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./assets/css'));
+});
